099f52253fe854016c63bb12f7d6b3610d9deaac
rain46688 <rain46688@gmail.com>
2022-08-18 14:12:35 +0900

    9, Lifecycle과 useEffect 사용법

--- src/routes/DetailComp.js
+++ src/routes/DetailComp.js
@@ -1,19 +1,40 @@
 import { useParams } from "react-router-dom";
 import data from "../data.js";
 import styled from 'styled-components'
+import { useEffect, useState } from "react";
+import Alert from 'react-bootstrap/Alert';
 
 // 이런식으로 styled를 이용해서 버튼을 만들어서 컴포넌트 처럼 사용 가능
 // 장점은 css 파일을 따로 만들지 않고 사용 가능하며 css에 props같은 react, js 문법을 넣을수있고 js 하나에 종속되서 로딩 개선
 // 종속 문제는 App.css 파일을 App.module.css 이런식으로 js 파일명 뒤에.module 붙이면 css가 js에 종속되게 할수도있으니 편한걸 쓰자
 // 단점은 코드가 복잡해지고 컴포넌트가 css인지 일반 컴포넌트인지 헷갈림
-let YellowBtn = styled.button`
-  background : ${ props => props.bg };
-  color : black;
-  padding : 10px;
-`;
+// let YellowBtn = styled.button`
+//   background : ${ props => props.bg };
+//   color : black;
+//   padding : 10px;
+// `;
+
+// 라이프 사이클
+// mount : 페이지 장착
+// update : 변경됨
+// unmount : 제거됨
 
 // 상세 화면 컴포넌트
 function DetailComp(props) {
+
+  // 랜더링 될때마다 실행됨
+  // 하지만 log를 useEffect 밖으로 빼도 똑같이 실행된다 이건 html 뿌려진 이후에 실행하는것이라
+  // 안에선 시간이 오래걸리는 디비 조회 같은 작업을 넣는게 좋음
+  useEffect(()=>{
+    console.log("디테일");
+    // 2초 후 배너 지우기
+    setTimeout(()=>{
+      setAlert(false);
+    },2000);
+  })
+
+  // let [count, setCount] = useState(0);
+  let [alert,setAlert] = useState(true);
 
   // url 파라미터 가져오기
   let {id} = useParams();
@@ -26,8 +47,18 @@ function DetailComp(props) {
 
     return (
       <>
-      <YellowBtn bg="orange">styled버튼</YellowBtn>
+      {/* <YellowBtn bg="orange">styled버튼</YellowBtn> */}
         <div className="container">
+          {
+            alert == true ?  
+            <Alert variant="warning">
+              <Alert.Heading>신규 알림! 2초 후 배너는 자동 종료됩니다.</Alert.Heading>
+            </Alert> : null
+          }
+
+          {/* {count}
+          <button onClick={()=>{setCount(count+1)}}>버튼</button> */}
+          <div ></div>
           <div className="row">
             <div className="col-md-6">
               <img

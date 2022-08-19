d816dbc33944b57acdb61586bcfb9bb057ed904f
rain46688 <rain46688@gmail.com>
2022-08-19 14:25:12 +0900

    17, redux 적용 장바구니 구현

--- src/routes/Cart.js
+++ src/routes/Cart.js
@@ -1,27 +1,53 @@
 import Table from 'react-bootstrap/Table'
+// {} 안붙이면 에러남 이유는 모르겠음..
+import { useSelector } from 'react-redux'
+import Button from 'react-bootstrap/Button';
 
+// 카트 컴포넌트
 function Cart(){
+
     return(
         <div>
-            <Table>
-                <thead>
-                    <tr>
-                    <th>#</th>
-                    <th>상품명</th>
-                    <th>수량</th>
-                    <th>변경하기</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <td>1</td>
-                    <td>안녕</td>
-                    <td>안녕</td>
-                    <td>안녕</td>
-                    </tr>
-                </tbody>
-            </Table>
+           <CartItems/>
         </div>
+    )
+}
+
+// 카트 아이템 컴포넌트
+function CartItems(){
+
+     // redux 사용법
+     let item = useSelector((state => {
+        // state.user 를 return하면 user만 가져올수잇음
+        return state.item
+    }));
+    console.log(item);
+
+    return(
+        <Table>
+        <thead>
+            <tr>
+            <th>#</th>
+            <th>상품명</th>
+            <th>수량</th>
+            <th>변경하기</th>
+            </tr>
+        </thead>
+        <tbody>
+            {
+                item.map((item, index)=>{
+                        return(
+                            <tr key={index}>
+                            <td>{item.id}</td>
+                            <td>{item.name}</td>
+                            <td>{item.count}</td>
+                            <td><Button variant="primary">변경하기</Button></td>
+                            </tr>
+                        )
+                })
+            }
+        </tbody>
+    </Table>
     )
 }
 
--- src/store.js
+++ src/store.js
@@ -1,5 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+
+// 상단에서 각각 사용하고자하는 데이터를 만듬 name, initialState 이렇게 작성해야됨
+let user = createSlice({
+    name : 'user',
+    initialState : 'kim'
+})
+
+let stock = createSlice({
+    name : 'stock',
+    initialState : [10, 11, 12]
+})
+
+let item = createSlice({
+    name : 'item',
+    initialState : [
+        {id : 0, name : 'White and Black', count : 2},
+        {id : 2, name : 'Grey Yordan', count : 1}
+      ]
+})
 
 export default configureStore({
-  reducer: { }
+    // 여기서 등록해줘야됨
+  reducer: { 
+    user : user.reducer,
+    stock : stock.reducer,
+    item : item.reducer,
+  }
 })

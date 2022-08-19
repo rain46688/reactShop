8c913502b551363af2fac5b7d3be8a40811b787b
rain46688 <rain46688@gmail.com>
2022-08-19 16:42:28 +0900

    18, state redux로 수정하는법

--- src/routes/Cart.js
+++ src/routes/Cart.js
@@ -1,13 +1,23 @@
 import Table from 'react-bootstrap/Table'
 // {} 안붙이면 에러남 이유는 모르겠음..
-import { useSelector } from 'react-redux'
+// redux로 수정하려면 useDispatch를 넣어주고 수정 함수를 호출해야됨
+import { useSelector, useDispatch } from 'react-redux'
 import Button from 'react-bootstrap/Button';
+import {changeName, changeCount} from './../store.js';
+
 
 // 카트 컴포넌트
 function Cart(){
 
+    let user = useSelector((state => {
+        // state.user 를 return하면 user만 가져올수잇음
+        return state.user
+    }));
+    console.log(user);
+
     return(
         <div>
+            {user} 의 장바구니
            <CartItems/>
         </div>
     )
@@ -15,6 +25,9 @@ function Cart(){
 
 // 카트 아이템 컴포넌트
 function CartItems(){
+
+    // redux 수정함수 호출 방법
+    let dispatch = useDispatch();
 
      // redux 사용법
      let item = useSelector((state => {
@@ -41,7 +54,11 @@ function CartItems(){
                             <td>{item.id}</td>
                             <td>{item.name}</td>
                             <td>{item.count}</td>
-                            <td><Button variant="primary">변경하기</Button></td>
+                            <td><Button onClick={()=>{
+                                // 수정 함수는 dispatch로 감싸서 호출
+                                // dispatch(changeName());
+                                dispatch(changeCount(item.id))
+                            }}variant="primary">+</Button></td>
                             </tr>
                         )
                 })
--- src/store.js
+++ src/store.js
@@ -3,7 +3,15 @@ import { configureStore, createSlice } from '@reduxjs/toolkit'
 // 상단에서 각각 사용하고자하는 데이터를 만듬 name, initialState 이렇게 작성해야됨
 let user = createSlice({
     name : 'user',
-    initialState : 'kim'
+    initialState : 'kim',
+
+    // state 수정 함수
+    reducers : {
+        changeName(state, i){ // state 기존 state
+            console.log("state : ",state);
+            return 'john '+state;// 이걸로 수정해줌
+        }
+    }
 })
 
 let stock = createSlice({
@@ -16,8 +24,27 @@ let item = createSlice({
     initialState : [
         {id : 0, name : 'White and Black', count : 2},
         {id : 2, name : 'Grey Yordan', count : 1}
-      ]
+      ],
+
+    reducers : {
+        // 클릭하면 맞는 상품의 count가 증가하도록 만든것
+        changeCount(state, num){
+            // console.log("state : ",state[0].count);
+            // console.log("num : ",num.payload);
+            
+            for(let i in state)
+                if(state[i].id == num.payload)
+                    state[i].count++;
+
+            return state;
+        }
+    }
 })
+
+
+// 함수 내보내는 방법 이렇게 해야지만 다른곳에서 호출할 수 있음
+export let {changeName} = user.actions;
+export let {changeCount} = item.actions;
 
 export default configureStore({
     // 여기서 등록해줘야됨
